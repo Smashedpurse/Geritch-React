8cd9dcdfc169d4bdbfd6466eaf7180971312417f
Ivan Rubio <smashedpurse@gmail.com>
2023-02-14 14:44:32 -0600

    Develope the basic sctructure of the Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineMenu } from "react-icons/md";
+import images from '../../constants/images'
+import "./Navbar.css";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+  <nav className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="app-logo"/>
+    </div>
+    <ul className="app__nabvar-links">
+    <li className="p__opensans"> <a href="#home">Home</a></li>
+    <li className="p__opensans"> <a href="#About">About</a></li>
+    <li className="p__opensans"> <a href="#Menu">Menu</a></li>
+    <li className="p__opensans"> <a href="#Awards">Awards</a></li>
+    <li className="p__opensans"> <a href="#Contact">Contact</a></li>
+    </ul>
+  </nav>
   )
-}
+};
 
-export default NavBar
+export default NavBar;
