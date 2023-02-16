a028cbfb65636106cde2783d533291aef9c94db9
Ivan Rubio <smashedpurse@gmail.com>
2023-02-16 09:28:00 -0600

    Develope the iteration of the second cocktails information of the data file and correct the name of the Carpet of MenuItem to recibe the information from the props correctly

--- src/containers/Menu/SpecialMenu.jsx
+++ src/containers/Menu/SpecialMenu.jsx
@@ -1,29 +1,47 @@
 import React from "react";
 import "./SpecialMenu.css";
-import { SubHeading, MenuItem} from "../../components";
+import { SubHeading, MenuItem } from "../../components";
 import images from "../../constants/images";
 import data from "../../constants/data";
 
-console.log(data)
+console.log(data);
 
 const SpecialMenu = () => {
   return (
     <div className="app__specialMenu flex__center section__padding" id="menu">
       <div className="app__specialMenu-title">
-        <SubHeading title={"Menu tha fits your Palatte"}/>
+        <SubHeading title={"Menu tha fits your Palatte"} />
         <h1 className="headtext__cormorant">Today's Special</h1>
       </div>
 
       <div className="app__specialMenu-menu">
-        <div className="app__specialMenu-menu_wine flex__center" >
+        <div className="app__specialMenu-menu_wine flex__center">
           <p className="app__specialMenu_menu_heading">Wine & Bear</p>
           <div className="app__specialMenu_menu_items">
-            {data.wines.map((wine,index) => (
-              <p>{wine.title}</p>
+            {data.wines.map((wine, index) => (
+              <MenuItem key={wine.title + index} title={wine.title} tags={wine.tags} price={wine.price}/>
+            ))}
+          </div>
+        </div> 
+
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="MenuIMG" />
+        </div>
+
+        <div className="app__specialMenu-menu_cocktails flex__center">
+          <p className="app__specialMenu_menu_heading">Cockta</p>
+          <div className="app__specialMenu_menu_items">
+            {data.cocktails.map((cocktail, index) => (
+              <MenuItem key={cocktail.title + index} title={cocktail.title} tags={cocktail.tags} price={cocktail.price}/>
             ))}
           </div>
         </div>
       </div>
+
+      <div style={{marginTop:"15px"}}>
+            <button type="button" className="custom__button">View More</button>
+      </div>
+
     </div>
   );
 };
