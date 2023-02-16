4726bd453ecc87920ad1c4a33446df8179adda36
Ivan Rubio <smashedpurse@gmail.com>
2023-02-16 09:05:56 -0600

    Iterate the first information of the wine from the data file

--- src/components/Menuitem/Menuitem.jsx
+++ src/components/Menuitem/Menuitem.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const Menuitem = () => {
+const Menuitem = ({title,price,tags}) => {
   return (
-    <div>Menuitem</div>
+    <div className='app__menuItem'>
+      <div className='app__menuItem-head'>
+        <div className='app__menuItem-name'>
+          <p className='p__cormorant' style={{color:'#DCCA87'}}>{title}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/containers/Menu/SpecialMenu.jsx
+++ src/containers/Menu/SpecialMenu.jsx
@@ -4,43 +4,25 @@ import { SubHeading, MenuItem} from "../../components";
 import images from "../../constants/images";
 import data from "../../constants/data";
 
+console.log(data)
+
 const SpecialMenu = () => {
   return (
     <div className="app__specialMenu flex__center section__padding" id="menu">
-    <div className="app__specialMenu-title">
-      <SubHeading title="Menu that fits your palatte" />
-      <h1 className="headtext__cormorant">Today&apos;s Special</h1>
-    </div>
-
-
-      <div className="app__specialMenu-menu">
-        <div className="app__specialMenu-menu_wine flex__center">
-          <p className="app__specialMenu_menu_heading">Wine & Beer</p>
-          <div className="app__specialMenu_menu_items">
-          {data.wines.map((wine, index) => (
-            <MenuItem Key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags} />
-          ))}
-          </div>
-        </div>
-
-        <div className="app__specialMenu-menu_img">
-          <img src={images.menu} alt="menu img" />
-        </div>
-
-        <div className="app__specialMenu-cocktails flex__center">
-          <p className="app__specialMenu_menu_heading">Wine & Beer</p>
-          <div className="app__specialMenu_menu_items">
-            {data.cocktails.map((cocktails, index) => {
-              return <p>{cocktails.title}</p>;
-            })}
-          </div>
-        </div>
+      <div className="app__specialMenu-title">
+        <SubHeading title={"Menu tha fits your Palatte"}/>
+        <h1 className="headtext__cormorant">Today's Special</h1>
       </div>
 
-      <div style={{ margin: "15px" }}>
-        <button type="button" className="custom__button">
-          View More
-        </button>
+      <div className="app__specialMenu-menu">
+        <div className="app__specialMenu-menu_wine flex__center" >
+          <p className="app__specialMenu_menu_heading">Wine & Bear</p>
+          <div className="app__specialMenu_menu_items">
+            {data.wines.map((wine,index) => (
+              <p>{wine.title}</p>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
