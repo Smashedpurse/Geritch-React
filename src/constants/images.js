13ecf71af60759303a1989533f5ed04c9a31a9e2
Ivan Rubio <smashedpurse@gmail.com>
2023-02-16 22:16:56 -0600

    Add the assets requiered for the Laurels Container

--- src/constants/images.js
+++ src/constants/images.js
@@ -13,10 +13,10 @@ import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
 // import findus from '../assets/findus.png';
 // import laurels from '../assets/laurels.png';
-// import award01 from '../assets/award01.png';
-// import award02 from '../assets/award02.png';
-// import award03 from '../assets/award03.png';
-// import award05 from '../assets/award05.png';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
 import sign from '../assets/sign.png';
 import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
@@ -37,10 +37,10 @@ export default {
    welcome,
   // findus,
   // laurels,
-  // award01,
-  // award02,
-  // award03,
-  // award05,
+  award01,
+  award02,
+  award03,
+  award05,
   sign,
   quote,
   gericht,
--- src/containers/Laurels/Laurels.jsx
+++ src/containers/Laurels/Laurels.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import './Laurels.css'
-import { SubHeading, MenuItem } from "../../components";
+import { SubHeading } from "../../components";
 import images from "../../constants/images";
 import data from "../../constants/data";
 
 const Laurels = () => {
   return (
-    <div>
+    <div className='app__bg app__wraper section__padding' id='awards'>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Awards & recognition"/>
+        <h1 className='headtext__cormorant'>Our Laurels</h1>
+      </div>
+
+      <div className='app_wrapper_img'>
+
+      </div>
 
     </div>
   )
