ef8c5ef84e15728619beba5bc8f87241b9da9e6f
Ivan Rubio <smashedpurse@gmail.com>
2023-02-17 09:51:20 -0600

    Realize the iteration of the data.awards and create a new component in the same Laurels.jsx to have a clean sintaxis of the iterarion

--- src/containers/Laurels/Laurels.jsx
+++ src/containers/Laurels/Laurels.jsx
@@ -4,18 +4,27 @@ import { SubHeading } from "../../components";
 import images from "../../constants/images";
 import data from "../../constants/data";
 
+const AwardCard = ({award:{imgUrl,title,subtitle}}) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award"/>
+  </div>
+);
+
 const Laurels = () => {
   return (
     <div className='app__bg app__wraper section__padding' id='awards'>
       <div className='app__wrapper_info'>
         <SubHeading title="Awards & recognition"/>
         <h1 className='headtext__cormorant'>Our Laurels</h1>
+
+        <div className='app__laurels_awards'>
+          {data.awards.map((award)=> <AwardCard award={award} key={award.title}/>)}
+        </div>
       </div>
 
       <div className='app_wrapper_img'>
 
       </div>
-
     </div>
   )
 
