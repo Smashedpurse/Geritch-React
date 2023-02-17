c684913eb85682a5801d2b251685a54fa40d8961
Ivan Rubio <smashedpurse@gmail.com>
2023-02-16 21:43:01 -0600

    Add the file Laurels to the Containers

---
+++ src/containers/Laurels/Laurels.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Laurels.css'
+import { SubHeading, MenuItem } from "../../components";
+import images from "../../constants/images";
+import data from "../../constants/data";
+
+const Laurels = () => {
+  return (
+    <div>
+
+    </div>
+  )
+
+}
+
+export default Laurels
