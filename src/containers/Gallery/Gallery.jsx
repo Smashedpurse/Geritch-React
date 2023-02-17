bfdaee62110d6e34f59080fd98b0d60017726c6c
Ivan Rubio <smashedpurse@gmail.com>
2023-02-17 12:05:37 -0600

    Develope the structure of the Gallery Container

--- src/containers/Gallery/Gallery.jsx
+++ src/containers/Gallery/Gallery.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './Gallery.css'
+import { SubHeading } from "../../components";
+import images from "../../constants/images";
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
 
 const Gallery = () => {
+
+
   return (
-    <div>Gallery</div>
+    <div className='app__gallery flex__center'>
+        <div className='app__gallery-content'>
+            <SubHeading title="Instagram"/>
+            <h1 className='headtext__cormorant'>Photo Gallery</h1>
+            <p className='p__opensans' style={{color:'#AAA',margin:'2rem'}}>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+            <button type='button' className='custom__button'>View More</button>
+        </div>
+    </div>
   )
 }
 
