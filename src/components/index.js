47d1e62fc99dbcfe8d01b647bfbecef89f4aef55
Ivan Rubio <smashedpurse@gmail.com>
2023-02-18 11:52:12 -0600

    Correction of MenuItem Component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import SubHeading from './SubHeading/SubHeading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './MenuItem/MenuItem';
+import MenuItem from './Menuitem/MenuItem';
 import NavBar from './NavBar/Navbar';
 import Footer from './Footer/FooterOverlay';
 
--- src/containers/Menu/SpecialMenu.jsx
+++ src/containers/Menu/SpecialMenu.jsx
@@ -4,8 +4,6 @@ import { SubHeading, MenuItem } from "../../components";
 import images from "../../constants/images";
 import data from "../../constants/data";
 
-console.log(data);
-
 const SpecialMenu = () => {
   return (
     <div className="app__specialMenu flex__center section__padding" id="menu">
